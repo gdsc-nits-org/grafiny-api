8c57ffa70f2081a50745f11bc9427f9c462bedaf
Anubhav <chakrabortyanubhav0@gmail.com>
2023-03-21 20:45:35 +0530

    ADD: user, profile, institute, items model, passport,

--- server.ts
+++ server.ts
@@ -2,6 +2,9 @@ import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import cookieSession from "cookie-session";
+import passport from "passport";
 
 import * as Middlewares from "./src/middlewares";
 import * as Routers from "./src/routers";
@@ -15,7 +18,19 @@ app
   .use(helmet())
   .use(morgan("dev"))
   .use(express.json())
-  .use(express.urlencoded({ extended: true }));
+  .use(express.urlencoded({ extended: true }))
+  .use(cookieParser())
+  .use(
+    cookieSession({
+      name: "session",
+      keys: [process.env.COOKIE_SECRET ?? "mysessionkey"],
+      maxAge: 2 * 24 * 60 * 60 * 1000,
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "prod",
+    })
+  )
+  .use(passport.initialize())
+  .use(passport.session());
 
 // Routers
 app.use(`${Constants.System.ROOT}/`, Routers.Health);
