7c19f9e97f3a1572a5accf8508aae2058c64ec06
Anubhav <chakrabortyanubhav0@gmail.com>
2023-06-05 10:48:56 +0530

    ADD: create insti func

---
+++ src/utils/institute/createInstitute.ts
@@ -0,0 +1,17 @@
+import prisma from "src/utils/prisma";
+
+const createInstitute = async (name: string) => {
+  try {
+    const institute = await prisma.institution.create({
+      data: {
+        name: name.toUpperCase(),
+      },
+    });
+    return institute;
+  } catch (err) {
+    console.log(err);
+    return null;
+  }
+};
+
+export default createInstitute;
