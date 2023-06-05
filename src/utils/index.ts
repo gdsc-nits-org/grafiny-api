5ba6650660851ea05287ab97ba5c995bac5e317a
Anubhav <chakrabortyanubhav0@gmail.com>
2023-06-05 11:15:12 +0530

    FIX: minor bugs

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,6 @@
 import * as Response from "./response";
 import prisma from "./prisma";
 import * as Auth from "./auth";
-import * as Institute from "./Institute";
+import * as Institute from "./institute";
 
 export { Response, prisma, Auth, Institute };
