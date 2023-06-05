e01ffafd00e48e75784fb43d32d8b4d20795eb67
Anubhav <chakrabortyanubhav0@gmail.com>
2023-06-05 11:04:23 +0530

    ADD: checkAdmin minddleware

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -3,10 +3,12 @@ import { invalidCredentials } from "src/globals/errors/auth";
 import isAuthenticated from "./authenticate";
 import validate from "./validate";
 import checkSuperAdmin from "./checkSuperAdmin";
+import checkAdmin from "./checkAdmin";
 export {
   Error,
   invalidCredentials,
   isAuthenticated,
   validate,
   checkSuperAdmin,
+  checkAdmin,
 };
