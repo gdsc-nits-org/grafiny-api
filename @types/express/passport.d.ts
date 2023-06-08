2f112fd57cd16a98bab321e5a5100b663876f616
Anubhav <chakrabortyanubhav0@gmail.com>
2023-06-08 23:20:50 +0530

    MODIFY user schema

--- @types/express/passport.d.ts
+++ @types/express/passport.d.ts
@@ -1,4 +1,4 @@
-import { User } from "@prisma/client";
+import { User, LEVEL } from "@prisma/client";
 
 declare global {
   namespace Express {
@@ -7,7 +7,7 @@ declare global {
       name: string;
       email: string;
       password: string;
-      isAdmin: number;
+      authorisationLevel: LEVEL;
     }
     export interface Request {
       session?: any;
--- src/utils/auth/db/createAdminUser.ts
+++ src/utils/auth/db/createAdminUser.ts
@@ -1,15 +1,17 @@
 import hashedPassword from "../passwordhash";
 import prisma from "src/utils/prisma";
 import { User } from "src/interfaces";
+import { LEVEL } from "@prisma/client";
 
 const createAdminUser = async (user: User) => {
   const hash = hashedPassword(user.password);
+  const status: LEVEL = "ONE";
   const createdUser = await prisma.user.create({
     data: {
       name: user.name,
       email: user.email,
       password: hash,
-      isAdmin: 1,
+      authorisationLevel: status,
     },
   });
   return createdUser;
@@ -17,12 +19,13 @@ const createAdminUser = async (user: User) => {
 
 const createSuperAdminUser = async (user: User) => {
   const hash = hashedPassword(user.password);
+  const status: LEVEL = "TWO";
   const createdUser = await prisma.user.create({
     data: {
       name: user.name,
       email: user.email,
       password: hash,
-      isAdmin: 2,
+      authorisationLevel: status,
     },
   });
   return createdUser;
